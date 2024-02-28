1c0a48ab63653baa265c198b496a20a872db4776
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-02-28 12:37:39 +0500

    Updated File

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 "use strict";
+// Variables
 const elForm = document.querySelector(".form");
 const elInput = document.querySelector(".input");
 const elList = document.querySelector(".todos-list");
 
+// Arrays
 const todos = [];
 
-const renderTodos = function (arr, htmlElement){
+// Render Todos function
+const renderTodos = function (arr, htmlElement) {
   htmlElement.innerHTML = "";
   arr.forEach((todo) => {
     const newLi = document.createElement("li");
@@ -13,27 +16,60 @@ const renderTodos = function (arr, htmlElement){
     const newDeleteBtn = document.createElement("button");
 
     newCheckBox.type = "checkbox";
-    newDeleteBtn.textContent = 'Delete';
+    newDeleteBtn.textContent = "Delete";
     newLi.innerHTML = todo.title;
 
+    // ClassName
+    newDeleteBtn.classList.add("delete-btn");
+    newCheckBox.classList.add("checkbox-btn");
+
+    // DataSet
+    newDeleteBtn.dataset.deleteBtnId = todo.id;
+    newCheckBox.dataset.checkboxBtnId = todo.id;
+
+    if (todo.isCompleted) {
+      newCheckBox.checked = true;
+      newLi.style.textDecoration = "line-through";
+      newLi.style.color = "gray";
+    }
+
+    // Appends
     htmlElement.appendChild(newLi);
     newLi.appendChild(newCheckBox);
     newLi.appendChild(newDeleteBtn);
   });
-}
+};
+
+// DOM
+elList.addEventListener("click", (e) => {
+  const deleteBtnId = e.target.dataset.deleteBtnId * 1;
+  const foundTodoIndex = todos.findIndex((todo) => todo.id === deleteBtnId);
+
+  if (e.target.matches(".delete-btn")) {
+    todos.splice(foundTodoIndex, 1);
+    elList.innerHTML = null;
+    renderTodos(todos, elList);
+  } else if (e.target.matches(".checkbox-btn")) {
+    const checkboxBtnId = e.target.dataset.checkboxBtnId * 1;
+    const foundTodo = todos.find((todo) => todo.id === checkboxBtnId);
+    foundTodo.isCompleted = !foundTodo.isCompleted;
+    renderTodos(todos, elList);
+  }
+});
+
 elForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const inputValue = elInput.value;
 
   const newTodo = {
-    id: todos.length,
+    id: todos[todos.length - 1]?.id + 1 || 0,
     title: inputValue,
     isCompleted: false,
   };
 
   todos.push(newTodo);
 
-  elInput.value = null
-  renderTodos(todos,elList)
+  elInput.value = null;
+  renderTodos(todos, elList);
 });
